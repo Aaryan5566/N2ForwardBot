bbb9912d8598d5e7de011a9b3de2a094be608a25
Aaryan5566 <arihansharma5467@gmail.com>
2025-03-13 13:41:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "25163484")
-    API_HASH = os.environ.get("API_HASH", "145bcbc424d1c1ffe04f3e607ea55c9a")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7062533535:AAGwGXeYVJXvWuoTyAi6ZETTbL26NXjL9zQ") 
+    API_ID = os.environ.get("API_ID", "23378704")
+    API_HASH = os.environ.get("API_HASH", "15a02b4d02babeb79e8f328b0ead0c17")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://fowardbot:fowardbot@cluster0.qjwunlh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://heckly5566:OKUxbQfVrIezLpra@cluster0.9hl7r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "Cluster0")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6302921275').split()]
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7364818327').split()]
 
 class temp(object): 
     lock = {}
